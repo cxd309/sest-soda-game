fefd208b260d738cec3c5ba910dd82ab02b93ecc
Chris Davis <cxd309@student.bham.ac.uk>
2025-04-03 14:31:46 +0100

    load sql.js

--- fs.js
+++ fs.js
@@ -5,10 +5,15 @@ import url from 'url';
 import fs from 'fs';
 import mime from 'mime/lite';
 
+const PORT = 8080;
+
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "./docs" + q.pathname;
-  console.log(filename)
+  if (q.pathname=="/"){
+    filename = "./docs/index.html";
+  }
+  console.log(q.pathname+" -> "+filename);
   fs.readFile(filename, function(err, data) {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
@@ -18,4 +23,6 @@ http.createServer(function (req, res) {
     res.write(data);
     return res.end();
   });
-}).listen(8080);
+}).listen(PORT);
+
+console.log("listening at http://localhost:"+PORT);
