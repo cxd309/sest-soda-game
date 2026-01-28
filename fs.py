d97fcd8080091fdf8cf67059da3809f05eb60e1a
Chris Davis <chris.davis.ee@gmail.com>
2026-01-28 10:15:51 +0000

    Use flask default static routing rather than doing my own faff

--- fs.py
+++ fs.py
@@ -1,34 +1,16 @@
 # File server to test github pages site
 
-from flask import Flask, send_from_directory, abort, Response
-from werkzeug.exceptions import NotFound
-import os
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
-PORT: int = 8080
-DOCS_DIR: str = "./docs"
+DOCS_DIR: str = "docs"
+
+app = Flask(__name__, static_folder=DOCS_DIR, static_url_path="")
 
 
-@app.route("/", defaults={"path": ""})
-@app.route("/<path:path>")
-def serve_file(path: str) -> Response:
-    # Default to index.html for root path
-    if path == "":
-        path = "index.html"
-
-    print(f"/{path} -> {os.path.join(DOCS_DIR, path)}")
-
-    try:
-        return send_from_directory(DOCS_DIR, path)
-    except FileNotFoundError:
-        abort(404)
-
-
-@app.errorhandler(404)
-def not_found(e: NotFound) -> tuple[str, int]:
-    return "404 Not Found", 404
+@app.route("/")
+def index():
+    return send_from_directory("docs", "index.html")
 
 
 if __name__ == "__main__":
-    print(f"listening at http://localhost:{PORT}")
-    app.run(port=PORT, debug=True)
+    app.run()
